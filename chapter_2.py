2ebc4bf1079e51c7815166f666281020b43851e7
Ulugbek Djuraev <ulugbek.dj@gmail.com>
2025-10-12 12:45:23 -0400

    added sections 2.5-2.8

--- chapter_2.py
+++ chapter_2.py
@@ -66,7 +66,7 @@ print(ids)
 # Decode (from ids) to text
 print(tokenizer.decode(ids))
 # %%
-#* Adding special context tokens
+#* 2.4 Adding special context tokens
 all_tokens = sorted(list(set(preprocessed)))
 all_tokens.extend(["<|endoftext|>", "<|unk|>"])
 
@@ -101,4 +101,139 @@ class SimpleTokenizerV2:
     
 tokenizer = SimpleTokenizerV2(vocab)
 print(tokenizer.decode(tokenizer.encode("Hello")))
+# %%
+#* Testing special tokens
+text = "Hello, do you like tea? Is this__ a test?"
+
+tokenizer.encode(text=text)
+# %%
+#* 2.5 Byte pair encoding
+import tiktoken
+
+tokenizer = tiktoken.get_encoding(encoding_name="gpt2")
+# %%
+print(tokenizer.encode(text="Hello world"))
+tokenizer.decode(tokens=tokenizer.encode(text="Hello world"))
+# %%
+text = (
+    "Hello, do you like marmalade? <|endoftext|> in snlit terraces..."
+)
+tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+# %%
+#* 2.6 Data sampling with a sliding window
+file_path = pb.Path("datasets", "the-verdict.txt")
+
+with open(file=file_path, encoding="utf-8") as f:
+    raw_text = f.read()
+    
+enc_text = tokenizer.encode(raw_text)
+# %%
+enc_sample = enc_text[50:]
+
+context_size = 4
+
+x = enc_sample[:context_size]
+y = enc_sample[1:context_size+1]
+
+print(f"x: {x}")
+print(f"y:      {y}")
+# %%
+for i in range(1, context_size+1):
+    context = enc_sample[:i]
+    desired = enc_sample[i]
+    
+    print(tokenizer.decode(context), "----->", tokenizer.decode([desired]))
+# %%
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+class GPTDatasetV1(Dataset):
+    def __init__(self, text, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+        
+        # Tokenize the entire text
+        token_ids = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+        
+        # Use a sliding window to chunk the book into overlapping sequences of max length
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i:i + max_length]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+            
+    def __len__(self):
+        return len(self.input_ids)
+    
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+
+# %%
+def create_dataloader_v1(txt, batch_size=4, max_length=256,
+                         stride=128, shuffle=True, drop_last=True,
+                         num_workers=0):
+    
+    # Initialize tokenizer
+    tokenizer = tiktoken.get_encoding("gpt2")
+    
+    # Create dataset
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+    
+    # Create dataloader
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers
+    )
+    
+    return dataloader
+
+# %%
+data_loader = create_dataloader_v1(raw_text, batch_size=1, max_length=4, stride=4, shuffle=False)
+
+data_iter = iter(data_loader)
+first_batch = next(data_iter)
+
+print(first_batch)
+# %%
+#* 2.7 Creating token embeddings
+input_ids = torch.tensor([2, 3, 5, 1])
+
+vocab_size = 6
+output_dim = 3
+
+torch.manual_seed(123)
+
+embedding_layer = torch.nn.Embedding(num_embeddings=vocab_size, 
+                                      embedding_dim=output_dim)
+# %%
+embedding_layer(torch.tensor(input_ids))
+# %%
+#* 2.8 Encoding word positions
+vocab_size = 50257
+output_dim = 256
+
+token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+
+# %%
+max_length = 4
+data_loader = create_dataloader_v1(raw_text, batch_size=8, max_length=max_length,
+                                   stride=max_length, shuffle=False)
+
+data_iter = iter(data_loader)
+inputs, targets = next(data_iter)
+
+# %%
+token_embeddings = token_embedding_layer(inputs)
+# %%
+# Positional embedding to help with word positions in a sentence
+context_length = max_length
+pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+# %%
+pos_embedding = pos_embedding_layer(torch.arange(context_length))
+# %%
+# now, input embedding vector - combination of words + positional embedding
+input_embedding = token_embeddings + pos_embedding
 # %%
